fd06e44f3543ff2e96b5e3c55ad74fa244d99795
emissan <ernesto.m.sanchez@outlook.com>
2024-09-12 19:01:55 -0400

    Add a clock object to be able to set the max FPS of the game to use less resources

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = clock.tick()/1000
     while True:
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        clock.tick(60)
 
 if __name__ == "__main__":
     main()
