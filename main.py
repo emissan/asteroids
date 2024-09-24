4123df212bdbca3df4c0522f0e94fba9dbe13d04
emissan <ernesto.m.sanchez@outlook.com>
2024-09-23 20:57:28 -0400

    Add shot.py and modify constants.py, main.py, and player.py to include ability to shoot

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -21,11 +22,13 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     # Add the different classes to groups
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
+    Shot.containers = (updatable, drawable)
 
     # Player and asteroid field created
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
